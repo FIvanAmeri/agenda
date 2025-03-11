cc391576c2b216e421b07a64c808895051425eea
Ivan Ameri <dark.ranked@gmail.com>
2025-03-11 13:23:00 -0300

    Elimino carpeta Add y quito comentarios

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import Form from './Form/page'; // Aquí importas tu componente de formulario.
-import { ObrasSocialesProvider } from "./context/ObrasSocialesContext"; // Asegúrate de que el contexto esté bien importado.
+import Form from './Form/page';
+import { ObrasSocialesProvider } from "./context/ObrasSocialesContext"; 
 
 export default function Page() {
   return (
     <ObrasSocialesProvider>
       <div className="min-h-screen flex items-center justify-center">
-        <Form /> {/* Aquí es donde se incluye el formulario o cualquier componente relevante */}
+        <Form />
       </div>
     </ObrasSocialesProvider>
   );
