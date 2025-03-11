2caaf92b69254142c4f39104313bd01dad70a8da
Ivan Ameri <dark.ranked@gmail.com>
2025-03-11 14:45:50 -0300

    Modifico CORS

--- back/src/index.ts
+++ back/src/index.ts
@@ -10,17 +10,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
+app.use(express.json());
 
 AppDataSource.initialize()
   .then(() => {
     console.log('Conexión con la base de datos establecida correctamente');
 
-
     app.use('/api/users', userRouter);
     app.use('/api', pacientesRouter);
+
     app.listen(port, () => {
       console.log(`Servidor corriendo en http://localhost:${port}`);
     });
